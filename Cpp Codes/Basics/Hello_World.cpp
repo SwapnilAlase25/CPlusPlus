ace1af4bae573c0de2ecc1c47f9cad7120ce07a4
Swapnil <swapnilalase07@gmail.com>
2020-08-12 13:10:00 +0530

    Hello World.cpp

---
+++ Cpp Codes/Basics/Hello_World.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Hello_World.cpp
+// Author      : Swapnil Alase
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << " Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << " Iam Swapnil Alase !! " << endl;
+	return 0;
+}
